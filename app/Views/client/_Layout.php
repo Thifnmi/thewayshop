560ae5c53783f32ce67a506441749d7e39331aad
Dat <datpro2810@gmail.com>
2021-08-11 22:03:21 +0700

    Edit View

---
+++ app/Views/client/_Layout.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Site Metas -->
+    <title>ThewayShop - Ecommerce Bootstrap 4 HTML Template</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="<?= base_url() ?>/client/images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="<?= base_url() ?>/client/images/apple-touch-icon.png">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>/client/css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>/client/css/style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>/client/css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>/client/css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Start Main Top -->
+    <header class="main-header">
+        <!-- Start Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-default bootsnav">
+            <div class="container">
+                <!-- Start Header Navigation -->
+                <div class="navbar-header">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand" href="<?= base_url() ?>"><img src="<?= base_url() ?>/client/images/logo.png" class="logo" alt=""></a>
+                </div>
+                <!-- End Header Navigation -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
+                        <li class="nav-item active"><a class="nav-link" href="<?= base_url() ?>">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url() ?>/About">About Us</a></li>
+                        <li class="dropdown megamenu-fw">
+                            <a href="#" class="nav-link" data-toggle="dropdown">Product <i class="fad fa-chevron-down"></i></a>
+                            <ul class="dropdown-menu megamenu-content" role="menu">
+                                <li>
+                                    <div class="row">
+                                        <div class="col-menu col-md-3">
+                                            <h6 class="title">Top</h6>
+                                            <div class="content">
+                                                <ul class="menu-col">
+                                                    <li><a href="#">Jackets</a></li>
+                                                    <li><a href="#">Shirts</a></li>
+                                                    <li><a href="#">Sweaters & Cardigans</a></li>
+                                                    <li><a href="#">T-shirts</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <!-- end col-3 -->
+                                        <div class="col-menu col-md-3">
+                                            <h6 class="title">Bottom</h6>
+                                            <div class="content">
+                                                <ul class="menu-col">
+                                                    <li><a href="#">Swimwear</a></li>
+                                                    <li><a href="#">Skirts</a></li>
+                                                    <li><a href="#">Jeans</a></li>
+                                                    <li><a href="#">Trousers</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <!-- end col-3 -->
+                                        <div class="col-menu col-md-3">
+                                            <h6 class="title">Clothing</h6>
+                                            <div class="content">
+                                                <ul class="menu-col">
+                                                    <li><a href="#">Top Wear</a></li>
+                                                    <li><a href="#">Party wear</a></li>
+                                                    <li><a href="#">Bottom Wear</a></li>
+                                                    <li><a href="#">Indian Wear</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-menu col-md-3">
+                                            <h6 class="title">Accessories</h6>
+                                            <div class="content">
+                                                <ul class="menu-col">
+                                                    <li><a href="#">Bags</a></li>
+                                                    <li><a href="#">Sunglasses</a></li>
+                                                    <li><a href="#">Fragrances</a></li>
+                                                    <li><a href="#">Wallets</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <!-- end col-3 -->
+                                    </div>
+                                    <!-- end row -->
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url() ?>/Contact">Contact Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">My Account <i class="fad fa-user-circle"></i></a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+                <!-- Start Atribute Navigation -->
+                <div class="attr-nav">
+                    <ul>
+                        <li class="search"><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li class="side-menu"><a href="#">
+                                <i class="fa fa-shopping-bag"></i>
+                                <span class="badge">3</span>
+                            </a></li>
+                    </ul>
+                </div>
+                <!-- End Atribute Navigation -->
+            </div>
+            <!-- Start Side Menu -->
+            <div class="side">
+                <a href="#" class="close-side"><i class="fa fa-times"></i></a>
+                <li class="cart-box">
+                    <ul class="cart-list">
+                        <li>
+                            <a href="#" class="photo"><img src="<?= base_url() ?>/client/images/img-pro-01.jpg" class="cart-thumb" alt="" /></a>
+                            <h6><a href="#">Delica omtantur </a></h6>
+                            <p>1x - <span class="price">$80.00</span></p>
+                        </li>
+                        <li>
+                            <a href="#" class="photo"><img src="<?= base_url() ?>/client/images/img-pro-02.jpg" class="cart-thumb" alt="" /></a>
+                            <h6><a href="#">Omnes ocurreret</a></h6>
+                            <p>1x - <span class="price">$60.00</span></p>
+                        </li>
+                        <li>
+                            <a href="#" class="photo"><img src="<?= base_url() ?>/client/images/img-pro-03.jpg" class="cart-thumb" alt="" /></a>
+                            <h6><a href="#">Agam facilisis</a></h6>
+                            <p>1x - <span class="price">$40.00</span></p>
+                        </li>
+                        <li class="total">
+                            <a href="#" class="btn btn-default hvr-hover btn-cart">VIEW CART</a>
+                            <span class="float-right"><strong>Total</strong>: $180.00</span>
+                        </li>
+                    </ul>
+                </li>
+            </div>
+            <!-- End Side Menu -->
+        </nav>
+        <!-- End Navigation -->
+    </header>
+    <!-- End Main Top -->
+
+    <!-- Start Top Search -->
+    <div class="top-search">
+        <div class="container">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                <input type="text" class="form-control" placeholder="Search">
+                <span class="input-group-addon close-search"><i class="fa fa-times"></i></span>
+            </div>
+        </div>
+    </div>
+    <!-- End Top Search -->
+
+
+    <?= $this->renderSection('content_Shop') ?>
+    <!-- Start Instagram Feed  -->
+    <div class="instagram-box">
+        <div class="main-instagram owl-carousel owl-theme">
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-01.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-02.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-03.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-04.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-05.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-06.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-07.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-08.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-09.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="ins-inner-box">
+                    <img src="<?= base_url() ?>/client/images/instagram-img-10.jpg" alt="" />
+                    <div class="hov-in">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Instagram Feed  -->
+
+    <a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+    <!-- Start Footer  -->
+    <footer>
+        <div class="footer-main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <div class="footer-link">
+                            <h4>HELP</h4>
+                            <ul>
+                                <li><a href="<?= base_url() ?>/Home">Advise </a></li>
+                                <li><a href="<?= base_url() ?>/About">Shipping and Delivery</a></li>
+                                <li><a href="<?= base_url() ?>/About">Payment</a></li>
+                                <li><a href="<?= base_url() ?>/Contact">Returns</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <div class="footer-link">
+                            <h4>Collection</h4>
+                            <ul>
+                                <li><a href="<?= base_url() ?>/Home"> Home </a></li>
+                                <li><a href="<?= base_url() ?>/About">About Us</a></li>
+                                <li><a href="<?= base_url() ?>/About">Product</a></li>
+                                <li><a href="<?= base_url() ?>/Contact">Contact Us</a></li>
+                                <li><a href="<?= base_url() ?>/About">My Account</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+                        <div class="footer-link-contact">
+                            <h4>Contact Us</h4>
+                            <ul>
+                                <li>
+                                    <p><i class="fas fa-map-marker-alt"></i>Address: University of Transport Technology <br>54 Phố Triều Khúc, Thanh Xuân Nam, Thanh Xuân, Hà Nội<br></p>
+                                </li>
+                                <li>
+                                    <p><i class="fas fa-phone-square"></i>Phone: <a href="tel: 024 3854 4264"> 024 3854 4264</a></p>
+                                </li>
+                                <li>
+                                    <p><i class="fas fa-envelope"></i>Email: <a href="mailto:contactinfo@gmail.com">contactinfo@gmail.com</a></p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer  -->
+
+    <!-- Start copyright  -->
+    <div class="footer-copyright">
+        <p class="footer-company">All Rights Reserved &copy; 2021 <a href="#">The Way Shop</a> Design By :
+            <a href="#">Nhóm 2 69DCTT21</a>
+        </p>
+    </div>
+    <!-- End copyright  -->
+
+    <a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+    <!-- ALL JS FILES -->
+    <script src="<?= base_url() ?>/client/js/jquery-3.2.1.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/popper.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="<?= base_url() ?>/client/js/jquery.superslides.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/bootstrap-select.js"></script>
+    <script src="<?= base_url() ?>/client/js/inewsticker.js"></script>
+    <script src="<?= base_url() ?>/client/js/bootsnav.js."></script>
+    <script src="<?= base_url() ?>/client/js/images-loded.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/isotope.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/owl.carousel.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/baguetteBox.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/form-validator.min.js"></script>
+    <script src="<?= base_url() ?>/client/js/contact-form-script.js"></script>
+    <script src="<?= base_url() ?>/client/js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ app/Views/client/about.php
@@ -0,0 +1,200 @@
+<?= $this->extend('client/_Layout') ?>
+<?= $this->section('content_Shop') ?>
+<!-- Start All Title Box -->
+<div class="all-title-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>ABOUT US</h2>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= base_url() ?>/Home">Home</a></li>
+                    <li class="breadcrumb-item active">About Us</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End All Title Box -->
+
+<!-- Start About Page  -->
+<div class="about-box-main">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 class="noo-sh-title">We are <span>The Way Shop</span></h2>
+                <p>Welcome to The Way Shop, your number one source for all things jewels. We're dedicated to giving you the best of jewels, with a focus on rings, necklace ...
+                    Founded in 2021, The Way Shop has come a long way from its beginnings in HaNoi, VietNam. When we started out, our passion for jewels. We now serve customers all over the world, and are thrilled to be a part of the fashion industry.</p>
+                <p>We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions, please don't hesitate to contact us.</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="banner-frame"> <img class="img-thumbnail img-fluid" src="<?= base_url() ?>/client/images/about-img.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+        <div class="row my-5">
+            <div class="col-sm-6 col-lg-4">
+                <div class="service-block-inner">
+                    <h3>We are Trusted</h3>
+                    <p>We served million customer from all of the world and all of them trusted we. Come with me, you will be one of them </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="service-block-inner">
+                    <h3>We are Professional</h3>
+                    <p>We had 20 years experience in jewelry. Our staff trained very careful before go to work. Just try and enjoy!</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+                <div class="service-block-inner">
+                    <h3>We are Here</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                </div>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-12">
+                <h2 class="noo-sh-title">Meet Our Team</h2>
+            </div>
+            <div class="col-sm-6 col-lg-2" style="margin-left:20px">
+                <div class="hover-team">
+                    <div class="our-team"> <img src="<?= base_url() ?>/client/images/img-1.jpg" alt="" />
+                        <div class="team-content">
+                            <h3 class="title">Văn Thìn</h3> <span class="post">Web Developer</span>
+                        </div>
+                        <ul class="social">
+                            <li>
+                                <a href="#" class="fab fa-facebook"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="fab fa-twitter"></a>
+                            </li>
+                           
+                            <li>
+                                <a href="#" class="fab fa-youtube"></a>
+                            </li>
+
+                        </ul>
+                        <div class="icon"> <i class="fa fa-plus" aria-hidden="true"></i> </div>
+                    </div>
+                    <div class="team-description">
+                        <p>Team Leader</p>
+                        <p>Come from BacGiang, VietNam. Student at the University of Transport Technology</p>
+                    </div>
+                    <hr class="my-0">
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-2" style="margin-left:20px">
+                <div class="hover-team">
+                    <div class="our-team"> <img src="<?= base_url() ?>/client/images/img-2.jpg" alt="" />
+                        <div class="team-content">
+                            <h3 class="title">Trọng Đạt</h3> <span class="post">Web Developer</span>
+                        </div>
+                        <ul class="social">
+                            <li>
+                                <a href="#" class="fab fa-facebook"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="fab fa-twitter"></a>
+                            </li>
+                           
+                            <li>
+                                <a href="#" class="fab fa-youtube"></a>
+                            </li>
+                        </ul>
+                        <div class="icon"> <i class="fa fa-plus" aria-hidden="true"></i> </div>
+                    </div>
+                    <div class="team-description">
+                        <p> Front End Developer </p>
+                        <p> Come from HaNoi, Viet Nam. Student at the University of Transport Technology</p>
+                    </div>
+                    <hr class="my-0">
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-2" style="margin-left:20px">
+                <div class="hover-team">
+                    <div class="our-team"> <img src="<?= base_url() ?>/client/images/img-3.jpg" alt="" />
+                        <div class="team-content">
+                            <h3 class="title">Văn Trường</h3> <span class="post">Web Developer</span>
+                        </div>
+                        <ul class="social">
+                            <li>
+                                <a href="#" class="fab fa-facebook"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="fab fa-twitter"></a>
+                            </li>
+                           
+                            <li>
+                                <a href="#" class="fab fa-youtube"></a>
+                            </li>
+                        </ul>
+                        <div class="icon"> <i class="fa fa-plus" aria-hidden="true"></i> </div>
+                    </div>
+                    <div class="team-description">
+                        <p>Back End Developer</p>
+                        <p>Come from NinhBinh, Viet Nam. Student at the University of Transport Technology</p>
+                    </div>
+                    <hr class="my-0">
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-2" style="margin-left:20px">
+                <div class="hover-team">
+                    <div class="our-team"> <img src="<?= base_url() ?>/client/images/img-4.jpg" alt="" />
+                        <div class="team-content">
+                            <h3 class="title">Bích Hiền</h3> <span class="post">Web Developer</span>
+                        </div>
+                        <ul class="social">
+                            <li>
+                                <a href="#" class="fab fa-facebook"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="fab fa-twitter"></a>
+                            </li>
+                           
+                            <li>
+                                <a href="#" class="fab fa-youtube"></a>
+                            </li>
+                        </ul>
+                        <div class="icon"> <i class="fa fa-plus" aria-hidden="true"></i> </div>
+                    </div>
+                    <div class="team-description">
+                        <p>Tester</p>
+                        <p>Come from HaNoi, Viet Nam. Student at the University of Transport Technology</p>
+                    </div>
+                    <hr class="my-0">
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-2" style="margin-left:20px">
+                <div class="hover-team">
+                    <div class="our-team"> <img src="<?= base_url() ?>/client/images/img-5.jpg" alt="" />
+                        <div class="team-content">
+                            <h3 class="title">Tùng Anh</h3> <span class="post">Web Developer</span>
+                        </div>
+                        <ul class="social">
+                            <li>
+                                <a href="#" class="fab fa-facebook"></a>
+                            </li>
+                            <li>
+                                <a href="#" class="fab fa-twitter"></a>
+                            </li>
+                           
+                            <li>
+                                <a href="#" class="fab fa-youtube"></a>
+                            </li>
+                        </ul>
+                        <div class="icon"> <i class="fa fa-plus" aria-hidden="true"></i> </div>
+                    </div>
+                    <div class="team-description">
+                        <p>Hiring </p>
+                        <p> Come from BacNinh, Viet Nam. Student at the University of Transport Technology</p>
+                    </div>
+                    <hr class="my-0">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- End About Page -->
+<?= $this->endSection() ?>
