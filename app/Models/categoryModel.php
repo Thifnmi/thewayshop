2cf434bdf2e3b364b6094d24d3865592253a5c8c
Thifnmi <tuthin2k@gmail.com>
2021-08-21 00:26:29 +0700

    fix somthing

--- app/Models/categoryModel.php
+++ app/Models/categoryModel.php
@@ -10,10 +10,16 @@ class categoryModel extends Model
     protected $primaryKey = 'id';
 
     protected $returnType     = 'array';
-    protected $allowedFields = ['id', 'name', 'url'];
+    protected $allowedFields = ['id', 'name', 'url','meta_title'];
 
     public function getAllcategory()
     {
         return $this->findAll();
+    }
+
+    public function getCategoryIdByMetalTitle(string $cmeta)
+    {
+        return $this->where('meta_title', $cmeta)->find();
+        # code...
     }
 }
